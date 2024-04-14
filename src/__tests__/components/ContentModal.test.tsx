bbabfb5a77a1656f29af4a2cbfa6f2c6e8a77945
youngAizy <ayandamarotya@gmail.com>
2024-04-15 00:24:42 +0200

    test contentModal component

---
+++ src/__tests__/components/ContentModal.test.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+
+const setSelected = jest.fn();
+
+const mockData = {
+    7801:{
+        tagline: "Hello There",
+        overview: " Just another assassin on a killing spree",
+        release_date: "2024-01-23",
+    }
+}
+
+describe('Test useContext calls',()=>{
+    describe('Component loads with correct context data',()=>{
+        
+    })
+
+})
