8b42b6d1ff25bd7d195de69868072fdd834633c1
youngAizy <ayandamarotya@gmail.com>
2024-04-17 18:30:23 +0200

    usew useEffect to update processing state from prop

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container } from '@mui/material';
 import StyledBtn from './StyledBtn.tsx';
 import SigninModal from "./SigninModal.tsx";
 import ProgressOverlay from './ProgressOverlay.tsx';
 
 const NavBar = ({ dialogOpen, setDialogOpen, processing = false, authText = "Signing In...", children}) => {
-  const [processingForm, setProcessing] = useState(processing);
+  const [processingForm, setProcessing] = useState(false);
+
+  useEffect(()=>{
+    setProcessing(processing);
+  },[processing])
   return (
     <>    
       <Container className="flex" 
