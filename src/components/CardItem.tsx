a75b34444c616ab86682d79b970eea9e14fe2169
youngAizy <ayandamarotya@gmail.com>
2024-04-23 19:16:56 +0200

    make minor updates to CardItems and CardItem components

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -19,7 +19,7 @@ interface Props{
 const CardItem = (props: Props) => {
     return (
         <ContentModal contentStyle="card" type={props.type} id={props.id} title={props.title} faves={props.faves} isSearch={props.isSearch}>
-            {props.title && (<Badge badgeContent={props.rating} color={props.rating>6 ? 'primary':'secondary'} />)}
+            {props.rating && (<Badge badgeContent={props.rating} color={props.rating>6 ? 'primary':'secondary'} />)}
             {props.title ? 
                 (<img src={props.poster ? `${img_300}${props.poster}`: unavailable} alt='' className="poster"/>): 
                 (<Skeleton variant="rectangular" height={250}/> )}
--- src/components/Carditems.tsx
+++ src/components/Carditems.tsx
@@ -1,17 +1,16 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import CardItem from './CardItem.tsx';
-import { AppContext } from '../Context/AppContext.tsx';
+// import { AppContext } from '../Context/AppContext.tsx';
 
-function Carditems({data, loading, faves, type, is_Search}) {
-    const {setSearchContent} = useContext(AppContext);
-    const content = {};
-    const p = loading ? Array.from(new Array(2)): (data ? data: [null]);
+function CardItems({data, loading, faves, type, is_Search=false}) {
+    // const {setSearchContent} = useContext(AppContext);
+    // const content = {};
+    const p = loading ? Array.from(new Array(4)): (data ? data: [null]);
  
     return (
     <>
         {p[0] || loading ? p.map((item,index)=>{
-            console.log("Jeep",p[0])
-            if(p[0]) content[item.id] = item;
+            // if(p[0]) content[item.id] = item;
             return (
             <CardItem
                 key={item?.id ? item.id: index} id={item?.id ? item.id: index} 
@@ -19,16 +18,16 @@ function Carditems({data, loading, faves, type, is_Search}) {
                 title={item?.title || item?.name}
                 summary={item?.overview}
                 type={type || ""}
-                release_date={item?.release_date}
+                release_date={item?.release_date || item?.first_air_date}
                 rating={item?.vote_average}
                 faves={faves}
                 isSearch={is_Search}
             />)
             }): (<h2>No Results</h2>)
         }
-        {Object.keys(content).length >=1 && setSearchContent(content)}
+        {/* {Object.keys(content).length >=1 && setSearchContent(content)} */}
     </>
   )
 }
 
-export default Carditems;
+export default CardItems;
