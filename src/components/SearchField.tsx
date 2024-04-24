a4092234dd69c2ca72d54fa726113b1c195286e3
youngAizy <ayandamarotya@gmail.com>
2024-04-24 13:57:08 +0200

    extract type select to a separate component and pass adornment prop

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import {FormControl, InputAdornment,TextField,Select, MenuItem} from '@mui/material';
+import {FormControl, InputAdornment,TextField} from '@mui/material';
 import {styled} from '@mui/material/styles';
-import TvIcon from '@mui/icons-material/Tv';
-import MovieIcon from '@mui/icons-material/Movie';
-import PersonIcon from '@mui/icons-material/Person';
 
 
 const SearchInput = styled(TextField)({
@@ -39,7 +36,7 @@ const SearchInput = styled(TextField)({
 }
 )
 
-const SearchField = ({type, setType,searchText,setSearchText})=> {
+const SearchField = ({adornment = null,searchText,setSearchText})=> {
 
   return (
     <div className='search-wrapper'>
@@ -51,33 +48,7 @@ const SearchField = ({type, setType,searchText,setSearchText})=> {
               variant="outlined"
               InputProps={{
                 endAdornment: <InputAdornment position="end">
-                
-                <Select
-                    labelId="type-select-label"
-                    id="type-select-outlined"
-                    value={type}
-                    onChange={e => setType(e.target.value)}
-                    label="Type"
-                    sx={{
-                        color: 'white',
-                        lineHeight: '1em',
-                        '.MuiSelect-select':{
-                            padding: '6px 16px',
-                            paddingRight: '16px !important',
-                            minHeight: '1em'
-                        },
-                        '.MuiSelect-icon':{
-                            display: 'none'
-                        },
-                        '.MuiOutlinedInput-notchedOutline':{
-                            border: 'none'
-                        }
-                    }}
-                  >
-                    <MenuItem selected value={'movie'}><MovieIcon/></MenuItem>
-                    <MenuItem value={"tv"}><TvIcon/> </MenuItem>
-                    <MenuItem value={"people"}><PersonIcon/> </MenuItem>
-                  </Select>
+                  {adornment}
                 </InputAdornment>,
               }}
               size="small"
